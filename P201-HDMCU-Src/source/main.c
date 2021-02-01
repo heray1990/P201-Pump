9f30f94aa6cc0c7c19e49d8aa5686d478660ef6e
heray1990 <heray1990@hotmail.com>
2021-02-01 23:44:54 +0800

    1. Create a new Keil MDK project for P201-Pump Project. 2. Copy mcu/common and driver from "hc32l13x_ddl_Rev1.9.2 Lite.zip". 3. Add code to light up the LCD screen.

---
+++ P201-HDMCU-Src/source/main.c
@@ -0,0 +1,205 @@
+/******************************************************************************
+* Copyright (C) 2017, Huada Semiconductor Co.,Ltd All rights reserved.
+*
+* This software is owned and published by:
+* Huada Semiconductor Co.,Ltd ("HDSC").
+*
+* BY DOWNLOADING, INSTALLING OR USING THIS SOFTWARE, YOU AGREE TO BE BOUND
+* BY ALL THE TERMS AND CONDITIONS OF THIS AGREEMENT.
+*
+* This software contains source code for use with HDSC
+* components. This software is licensed by HDSC to be adapted only
+* for use in systems utilizing HDSC components. HDSC shall not be
+* responsible for misuse or illegal use of this software for devices not
+* supported herein. HDSC is providing this software "AS IS" and will
+* not be responsible for issues arising from incorrect user implementation
+* of the software.
+*
+* Disclaimer:
+* HDSC MAKES NO WARRANTY, EXPRESS OR IMPLIED, ARISING BY LAW OR OTHERWISE,
+* REGARDING THE SOFTWARE (INCLUDING ANY ACOOMPANYING WRITTEN MATERIALS),
+* ITS PERFORMANCE OR SUITABILITY FOR YOUR INTENDED USE, INCLUDING,
+* WITHOUT LIMITATION, THE IMPLIED WARRANTY OF MERCHANTABILITY, THE IMPLIED
+* WARRANTY OF FITNESS FOR A PARTICULAR PURPOSE OR USE, AND THE IMPLIED
+* WARRANTY OF NONINFRINGEMENT.
+* HDSC SHALL HAVE NO LIABILITY (WHETHER IN CONTRACT, WARRANTY, TORT,
+* NEGLIGENCE OR OTHERWISE) FOR ANY DAMAGES WHATSOEVER (INCLUDING, WITHOUT
+* LIMITATION, DAMAGES FOR LOSS OF BUSINESS PROFITS, BUSINESS INTERRUPTION,
+* LOSS OF BUSINESS INFORMATION, OR OTHER PECUNIARY LOSS) ARISING FROM USE OR
+* INABILITY TO USE THE SOFTWARE, INCLUDING, WITHOUT LIMITATION, ANY DIRECT,
+* INDIRECT, INCIDENTAL, SPECIAL OR CONSEQUENTIAL DAMAGES OR LOSS OF DATA,
+* SAVINGS OR PROFITS,
+* EVEN IF Disclaimer HAS BEEN ADVISED OF THE POSSIBILITY OF SUCH DAMAGES.
+* YOU ASSUME ALL RESPONSIBILITIES FOR SELECTION OF THE SOFTWARE TO ACHIEVE YOUR
+* INTENDED RESULTS, AND FOR THE INSTALLATION OF, USE OF, AND RESULTS OBTAINED
+* FROM, THE SOFTWARE.
+*
+* This software may be replicated in part or whole for the licensed use,
+* with the restriction that this Disclaimer and Copyright notice must be
+* included with each copy of this software, whether used in part or whole,
+* at all times.
+*/
+/******************************************************************************/
+/** \file main.c
+ **
+ ** A detailed description is available at
+ ** @link Sample Group Some description @endlink
+ **
+ **   - 2016-02-16  1.0  XYZ First version for Device Driver Library of Module.
+ **
+ ******************************************************************************/
+
+/******************************************************************************
+ * Include files
+ ******************************************************************************/
+#include "lcd.h"
+#include "lpm.h"
+#include "gpio.h"
+
+/******************************************************************************
+ * Local pre-processor symbols/macros ('#define')                            
+ ******************************************************************************/
+
+/******************************************************************************
+ * Global variable definitions (declared in header file with 'extern')
+ ******************************************************************************/
+
+/******************************************************************************
+ * Local type definitions ('typedef')                                         
+ ******************************************************************************/
+
+/******************************************************************************
+ * Local function prototypes ('static')
+ ******************************************************************************/
+
+/******************************************************************************
+ * Local variable definitions ('static')                                      *
+ ******************************************************************************/
+
+/******************************************************************************
+ * Local pre-processor symbols/macros ('#define')                             
+ ******************************************************************************/
+
+/*****************************************************************************
+ * Function implementation - global ('extern') and local ('static')
+ ******************************************************************************/
+void App_PortCfg(void);
+void App_LcdCfg(void);
+/**
+ ******************************************************************************
+ ** \brief  主函数
+ ** 
+ ** @param  无
+ ** \retval 无
+ **
+ ******************************************************************************/ 
+int32_t main(void)
+{
+    Sysctrl_ClkSourceEnable(SysctrlClkRCL,TRUE);            ///< 使能RCL时钟
+    Sysctrl_SetRCLTrim(SysctrlRclFreq32768);                ///< 配置内部低速时钟频率为32.768kHz
+
+    Sysctrl_SetPeripheralGate(SysctrlPeripheralLcd,TRUE);   ///< 开启LCD时钟
+    Sysctrl_SetPeripheralGate(SysctrlPeripheralGpio,TRUE);  ///< 开启GPIO时钟
+
+    App_PortCfg();               ///< LCD端口配置
+    App_LcdCfg();                ///< LCD模块配置
+
+    Lcd_ClearDisp();             ///< 清屏
+    Lcd_WriteRam(0,0xffffffff);  ///< 赋值寄存器LCDRAM0
+    Lcd_WriteRam(1,0xffffffff);  ///< 赋值寄存器LCDRAM1
+    Lcd_WriteRam(2,0xffffffff);  ///< 赋值寄存器LCDRAM2
+    Lcd_WriteRam(3,0xffffffff);  ///< 赋值寄存器LCDRAM3
+    Lcd_WriteRam(4,0xffffffff);  ///< 赋值寄存器LCDRAM4
+    Lcd_WriteRam(5,0x00ffffff);  ///< 赋值寄存器LCDRAM5
+
+    while(1)
+    {
+        ;
+    }
+}
+
+/**
+ ******************************************************************************
+ ** \brief  初始化外部GPIO引脚
+ **
+ ** \return 无
+ ******************************************************************************/
+void App_PortCfg(void)
+{
+    Gpio_SetAnalogMode(GpioPortA, GpioPin9);  //COM0
+    Gpio_SetAnalogMode(GpioPortA, GpioPin10); //COM1
+    Gpio_SetAnalogMode(GpioPortA, GpioPin11); //COM2
+    Gpio_SetAnalogMode(GpioPortA, GpioPin12); //COM3
+    Gpio_SetAnalogMode(GpioPortC, GpioPin10); //COM4
+    Gpio_SetAnalogMode(GpioPortC, GpioPin11); //COM5
+    Gpio_SetAnalogMode(GpioPortC, GpioPin12); //COM6
+    Gpio_SetAnalogMode(GpioPortD, GpioPin2); //COM7
+
+    Gpio_SetAnalogMode(GpioPortA, GpioPin8);  //SEG0
+    Gpio_SetAnalogMode(GpioPortC, GpioPin9);  //SEG1
+    Gpio_SetAnalogMode(GpioPortC, GpioPin8);  //SEG2
+    Gpio_SetAnalogMode(GpioPortC, GpioPin7);  //SEG3
+    Gpio_SetAnalogMode(GpioPortC, GpioPin6);  //SEG4
+    Gpio_SetAnalogMode(GpioPortB, GpioPin15); //SEG5
+    Gpio_SetAnalogMode(GpioPortB, GpioPin14); //SEG6
+    Gpio_SetAnalogMode(GpioPortB, GpioPin13); //SEG7
+    Gpio_SetAnalogMode(GpioPortB, GpioPin12); //SEG8
+    Gpio_SetAnalogMode(GpioPortB, GpioPin11); //SEG9
+    Gpio_SetAnalogMode(GpioPortB, GpioPin10); //SEG10
+    Gpio_SetAnalogMode(GpioPortB, GpioPin2); //SEG11
+    Gpio_SetAnalogMode(GpioPortB, GpioPin1); //SEG12
+    Gpio_SetAnalogMode(GpioPortB, GpioPin0); //SEG13
+    Gpio_SetAnalogMode(GpioPortC, GpioPin5); //SEG14
+    Gpio_SetAnalogMode(GpioPortC, GpioPin4); //SEG15
+    Gpio_SetAnalogMode(GpioPortA, GpioPin7); //SEG16
+    Gpio_SetAnalogMode(GpioPortA, GpioPin6); //SEG17
+    Gpio_SetAnalogMode(GpioPortA, GpioPin5); //SEG18
+    Gpio_SetAnalogMode(GpioPortA, GpioPin4); //SEG19
+    Gpio_SetAnalogMode(GpioPortA, GpioPin3); //SEG20
+    Gpio_SetAnalogMode(GpioPortA, GpioPin2); //SEG21
+    Gpio_SetAnalogMode(GpioPortA, GpioPin1); //SEG22
+    Gpio_SetAnalogMode(GpioPortB, GpioPin3); //SEG32/VLCD1
+    Gpio_SetAnalogMode(GpioPortB, GpioPin4); //SEG33/VLCD2
+    Gpio_SetAnalogMode(GpioPortB, GpioPin5); //SEG34/VLCD3
+    Gpio_SetAnalogMode(GpioPortB, GpioPin6); //SEG35/VLCDH
+}
+
+/**
+ ******************************************************************************
+ ** \brief  配置LCD
+ **
+ ** \return 无
+ ******************************************************************************/
+void App_LcdCfg(void)
+{
+    stc_lcd_cfg_t LcdInitStruct;
+    stc_lcd_segcom_t LcdSegCom;
+
+    LcdSegCom.u32Seg0_31 = 0xff800000;                              ///< 配置LCD_POEN0寄存器 开启SEG0~SEG22
+    LcdSegCom.stc_seg32_51_com0_8_t.seg32_51_com0_8 = 0xffffffff;   ///< 初始化LCD_POEN1寄存器 全部关闭输出端口
+    LcdSegCom.stc_seg32_51_com0_8_t.segcom_bit.Seg36Com7 = 0;       ///< 使能COM0~COM7
+    LcdSegCom.stc_seg32_51_com0_8_t.segcom_bit.Seg37Com6 = 0;
+    LcdSegCom.stc_seg32_51_com0_8_t.segcom_bit.Seg38Com5 = 0;
+    LcdSegCom.stc_seg32_51_com0_8_t.segcom_bit.Seg39Com4 = 0;
+    LcdSegCom.stc_seg32_51_com0_8_t.segcom_bit.Com0_3 = 0;
+    LcdSegCom.stc_seg32_51_com0_8_t.segcom_bit.Mux = 0;             ///< Mux=0,Seg32_35=0,BSEL=1表示:选择外部电容工作模式，内部电阻断路
+    LcdSegCom.stc_seg32_51_com0_8_t.segcom_bit.Seg32_35 = 0;
+    Lcd_SetSegCom(&LcdSegCom);                                      ///< LCD COMSEG端口配置
+
+    LcdInitStruct.LcdBiasSrc = LcdExtCap;                          ///< 电容分压模式，需要外部电路配合
+    LcdInitStruct.LcdDuty = LcdDuty8;                              ///< 1/8 Duty
+    LcdInitStruct.LcdBias = LcdBias3;                              ///< 1/3 BIAS
+    LcdInitStruct.LcdCpClk = LcdClk2k;                             ///< 电压泵时钟频率选择2kHz
+    LcdInitStruct.LcdScanClk = LcdClk128hz;                        ///< LCD扫描频率选择128Hz
+    LcdInitStruct.LcdMode = LcdMode0;                              ///< 选择模式0
+    LcdInitStruct.LcdClkSrc = LcdRCL;                              ///< LCD时钟选择RCL
+    LcdInitStruct.LcdEn   = LcdEnable;                             ///< 使能LCD模块
+    Lcd_Init(&LcdInitStruct);
+}
+
+
+/******************************************************************************
+ * EOF (not truncated)
+ ******************************************************************************/
+
+
